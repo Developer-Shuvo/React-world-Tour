603e72257165e7e624afb6923d1e42671b98e031
SHUVO <im.computer.engineeer@gamil.com>
2025-07-04 11:16:08 +0600

    World Tour Using React

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+
+
+const Countries = () => {
+    const [countries, setCountries]= useState([])
+useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+}, []);
+return (
+    <div>
+        <h2>Countries : {countries.length}</h2>
+        {
+         countries.map(country =><Country country={country}></Country> )
+        }
+    </div>
+);
+};
+
+export default Countries;
