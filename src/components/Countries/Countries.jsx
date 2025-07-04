b5687f00e6c4de211fe195494607ac1d366eff0f
SHUVO <im.computer.engineeer@gamil.com>
2025-07-04 20:00:13 +0600

    Use Grid and Manipulate Everything

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
-
+import "./Countries.css";
 
 const Countries = () => {
-    const [countries, setCountries]= useState([])
-useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population')
-        .then(res => res.json())
-        .then(data => setCountries(data));
-}, []);
-return (
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population"
+    )
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
     <div>
-        <h2>Countries : {countries.length}</h2>
-        {
-         countries.map(country =><Country
-            key={country.population.region} country={country}></Country> )
-        }
+      <h2>Countries : {countries.length}</h2>
+
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.population.region} country={country}></Country>
+        ))}
+      </div>
     </div>
-);
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import "./Country.css";
 
 const Country = ({ country }) => {
-  const { name,capital,region,flags,population } = country;
+  const { name, capital, region, flags, population } = country;
   return (
     <div className="country">
       {/* Country Name  */}
       <h3>
         Name : <span className="countryName">{name?.common} </span>{" "}
       </h3>
-      <h3>Capital : <span className="capital"> {capital}</span> </h3>
+      {/* Capital  */}
+      <h3>
+        Capital : <span className="capital"> {capital}</span>{" "}
+      </h3>
       {/* Population  */}
-      <h3>Population : <span className="population"> {population}</span> </h3>
-      <h3>Region : <span className="region">{region}</span> </h3>
+      <h3>
+        Population : <span className="population"> {population}</span>{" "}
+      </h3>
+      {/* Region  */}
+      <h3>
+        Region : <span className="region">{region}</span>{" "}
+      </h3>
       {/* //Flag  */}
       <img src={flags.png} alt="" />
     </div>
